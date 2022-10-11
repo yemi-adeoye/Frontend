dfb09212f300e370a17d9d8624267fbb135ad782
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-10-11 20:18:37 +0530

    calc op

--- angular-project/MyEmployeeUI/src/app/app.component.ts
+++ angular-project/MyEmployeeUI/src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyEmployeeUI';
+  x:number=20;
+  y:number=10;
+  msg:string; //undefined
+  result:number;
+
+  welcomeFunc(){
+    this.msg='Hey Welcome to angular';
+  }
+
+  taskFunc(){
+    this.msg="work with typescript";
+  }
+
+  calc(op:string){
+    switch(op){
+      case 'add':
+        this.result=this.x+this.y;
+        break;
+      case 'sub':
+        this.result=this.x-this.y;
+        break;
+    }
+  }
 }
+
+/*
+  number,string,boolean
+*/
