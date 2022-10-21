c003e305e4c86a37f534344b3ea58b4c96dd5838
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-10-21 22:36:12 +0530

    initial commit with employee and user model

---
+++ backend-playground/src/main/java/com/playground/api/BackendPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.playground.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPlaygroundApplication.class, args);
+	}
+
+}
