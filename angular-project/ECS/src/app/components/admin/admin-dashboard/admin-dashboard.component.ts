cee7339d469f40e9c6bab59c4b086ca870ff0732
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-10-29 02:00:16 +0530

    admin dashboard

--- angular-project/ECS/src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
+++ angular-project/ECS/src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
@@ -9,77 +9,96 @@ import {Employee} from '../../../models/employee.model'
 })
 export class AdminDashboardComponent   {
   employees: Employee[] =[];
+   data = [];
   constructor(private http: HttpClient) {}
   //array varibales to store csv data
  lines = []; //for headings
  linesR = []; // for rows
-
+   myFormData = new FormData();
  //File upload function
  changeListener(files: FileList){
-    if(files && files.length > 0) {
-      let file : File = files.item(0);
+  console.log(files);
+  if(files && files.length > 0) {
+     let file : File = files.item(0);
+       console.log(file.name);
+       console.log(file.size);
+       console.log(file.type);
+       let reader: FileReader = new FileReader();
+       reader.readAsText(file);
+       reader.onload = (e) => {
+        let csv: any = reader.result;
+        let allTextLines = [];
+        allTextLines = csv.split(/\r|\n|\r/);
+       // console.log(allTextLines);
 
-        let reader: FileReader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = (e) => {
-         let csv: any = reader.result;
-         let allTextLines = [];
-         allTextLines = csv.split(/\r|\n|\r/);
-
-        //Table Headings
-         let headers = allTextLines[0].split(';');
-         let data = headers;
-         let tarr = [];
-         for (let j = 0; j < headers.length; j++) {
-           tarr.push(data[j]);
-         }
-         //Pusd headinf to array variable
-         this.lines.push(tarr);
+       //Table Headings
+        let headers = allTextLines[0].split(';');
+        let data = headers;
+        let tarr = [];
+        for (let j = 0; j < headers.length; j++) {
+          tarr.push(data[j]);
+        }
+        //Pusd headinf to array variable
+        this.lines.push(tarr);
 
 
-         // Table Rows
-         let tarrR = [];
+        // Table Rows
+        let tarrR = [];
 
-         //Create formdata object
-         var myFormData = new FormData();
-         let arrl = allTextLines.length;
-         let rows = [];
+        //Create formdata object
 
-         for(let i = 1; i < arrl; i++){
-         rows.push(allTextLines[i].split(';'));
-         if(allTextLines[i]!=""){
+        let arrl = allTextLines.length;
+        let rows = [];
 
-         // Save file data into formdata varibale
-         myFormData.append("data"+i, allTextLines[i]);
-       }
-         }
+        for(let i = 1; i < arrl; i++){
+        rows.push(allTextLines[i].split(';'));
+        if(allTextLines[i]!=""){
 
-         for (let j = 0; j < arrl; j++) {
+        // Save file data into formdata varibale
+        this.myFormData.append("data"+i, allTextLines[i]);
+      }
+        }
+
+        for (let j = 0; j < arrl; j++) {
+
+
+
             tarrR.push(rows[j]);
-             tarrR = tarrR.filter(function(i){ return i != ""; });
-             // Begin assigning parameters
-          }
-        //Push rows to array variable
-         this.linesR.push(tarrR);
-         console.log(this.linesR);
-         for(let i=0;i<this.linesR.length;i++){
-          let e=this.linesR[i];
-          console.log(e[0][0].split(','));
-          console.log('------end---------')
-         }
-         this.linesR.forEach(e=>{
-            let employee:Employee = {
-              name: e.name,
-              jobTitle: e.jobTitle,
-              email: e.email,
-              password: e.password
-            }
-            console.log(employee);
-         })
+            tarrR = tarrR.filter(function(i){ return i != ""; });
 
 
-     }
-   }
- }
+
+            // Begin assigning parameters
+
+
+
+
+
+        }
+       //Push rows to array variable
+        this.linesR.push(tarrR);
+
+        console.log()
+
+
+    }
+  }
+
+  this.myFormData.forEach((x: File) => {
+    console.log(x);
+  });
 
 }
+
+}
+/*
+let employee:Employee = {
+            name: valArry[0],
+            jobTitle: valArry[3],
+            email: valArry[1],
+            password: valArry[2],
+            totalLeaves: valArry[4],
+            leavesLeft: valArry[5],
+            managerName: valArry[6]
+          };
+*/
